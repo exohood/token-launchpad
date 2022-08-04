c6bcf4568f1e0b128533b8c4d701db9f7c9402c8
Shira Otsuki <107924319+ShiraOtsuki@users.noreply.github.com>
2022-08-04 07:48:21 -0400

    Delete tron-token directory
    Exohood´s Protocol does not support any more TRON.

--- tron-token/scripts/all-token-tests.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-for file in "scripts/templated-tests/configs/token"/**/*
-do
-  echo "testing file "$file
-  source $(dirname "$0")/templated-tests/compile-token-template.sh $file && source $(dirname "$0")/templated-tests/test-token-template.sh $file || error "failed test "$file
-done
